ed383418c3b116a66e045c6177a1d63c66cca3e0
Naamee <naameeakram@gmail.com>
2024-03-06 15:13:04 +0500

    adjusted gradient

--- src/components/MainProjects.vue
+++ src/components/MainProjects.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col snap-start min-h-lvh w-full items-center justify-center text-white bg-gradient-358 from-black from-70% to-80% to-blue-950/30">
+    <div class="flex flex-col snap-start min-h-lvh w-full items-center justify-center text-white bg-gradient-358 from-black from-70% to-80% to-blue-950/25">
         <h1 class="text-5xl">Projects</h1>
         <p></p>
     </div>
