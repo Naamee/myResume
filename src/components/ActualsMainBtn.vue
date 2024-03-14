a0088f5f2ebf82bdae5f4c30fc529cc7c839e440
Naamee <naameeakram@gmail.com>
2024-03-14 09:15:46 +0500

    removed actualsmainbtn

--- src/components/ActualsMainBtn.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="flex justify-end">
-    <button class="text-white text-xs p-2 border border-white">
-        Documents
-    </button>
-  </div>
-</template>
--- src/components/MainEducation.vue
+++ src/components/MainEducation.vue
@@ -2,7 +2,6 @@
 import { ref, watch } from 'vue'
 import { useElementVisibility } from '@vueuse/core'
 import { useVisibilityStore } from '@/stores/visibilityStore';
-import ActualsMainBtn from '@/components/ActualsMainBtn.vue'
 
 const target = ref(null)
 const visibilityStore = useVisibilityStore()
@@ -19,33 +18,32 @@ watch(targetIsVisible, (isVisible) => {
         <!-- <h1 class="text-5xl">Education</h1> -->
         <!-- Main Container -->
         <div class="mt-5 text-center">
-            <ActualsMainBtn />
             <div class="divide-y divide-dashed divide-white/30">
                 <!-- Bachelors -->
                 <div class="flex flex-col rounded-sm p-3">
                     <p class="text-xs font-light text-white/50">2018 - 2021</p>
                     <p class="text-md">BSc (Hons) in Accounting and Finance with Specialism in Taxation</p>
                     <p class="text-sm">Asia Pacific University, Malaysia</p>
-                    <p class="text-xs text-amber-300">First Class Honors - CGPA 3.66</p>
+                    <!-- <p class="text-xs text-amber-300">First Class Honors - CGPA 3.66</p> -->
                 </div>
                 <!-- Higher Secondary Education -->
                 <div class="flex flex-col rounded-sm p-3">
                     <p class="text-xs font-light text-white/50">2015 - 2017</p>
                     <p class="text-md">Edexcel GCE & HSC</p>
                     <p class="text-sm">Center for Higher Education, Maldives</p>
-                    <p class="text-xs text-amber-300">Dhivehi - B, Islam - A, Statistics - A, Principle of
-                        Accounts - A, Business Studies - A, Economics - A*</p>
+                    <!-- <p class="text-xs text-amber-300">Dhivehi - B, Islam - A, Statistics - A, Principle of
+                        Accounts - A, Business Studies - A, Economics - A*</p> -->
                 </div>
                 <!-- Secondary Education -->
                 <div class="flex flex-col rounded-sm p-3">
                     <p class="text-xs font-light text-white/50">2012 - 2014</p>
                     <p class="text-md">SSC, IGCSE and GCSE</p>
                     <p class="text-sm">Kalaafaanu School, Maldives</p>
-                    <p class="text-xs text-amber-300">Dhivehi - B, Islam - A, English as a Second
+                    <!-- <p class="text-xs text-amber-300">Dhivehi - B, Islam - A, English as a Second
                         Language - A*, Mathematics - A, Principle of
                         Accounts - A*, Business Studies - A, Economics - A,
                         Computer Studies - A*
-                    </p>
+                    </p> -->
                 </div>
             </div>
         </div>
