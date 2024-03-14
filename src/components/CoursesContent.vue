591a2c858785d416dbeff563da5550ac095dc3bf
Naamee <naameeakram@gmail.com>
2024-03-14 11:40:49 +0500

    updated educaiton content

--- src/components/CoursesContent.vue
+++ src/components/CoursesContent.vue
@@ -1,24 +1,22 @@
+<script setup>
+const courses = [
+    {
+        year: '2018 - 2021',
+        course: 'BSc (Hons) in Accounting and Finance with Specialism in Taxation',
+        institution: 'Asia Pacific University, Malaysia'
+    },
+]
+</script>
+
 <template>
     <!-- Main Container -->
     <div class="mt-5 text-start border rounded-md text-xs w-72">
             <div>
                 <!-- Bachelors -->
-                <div class="flex flex-col rounded-sm p-3">
-                    <p class="font-light text-white/50">2018 - 2021</p>
-                    <p class="text-sm">BSc (Hons) in Accounting and Finance with Specialism in Taxation</p>
-                    <p class="italic">Asia Pacific University, Malaysia</p>
-                </div>
-                <!-- Higher Secondary Education -->
-                <div class="flex flex-col rounded-sm p-3">
-                    <p class="font-light text-white/50">2015 - 2017</p>
-                    <p class="text-sm">Edexcel GCE & HSC</p>
-                    <p class="italic">Center for Higher Education, Maldives</p>
-                </div>
-                <!-- Secondary Education -->
-                <div class="flex flex-col rounded-sm p-3">
-                    <p class="font-light text-white/50">2012 - 2014</p>
-                    <p class="text-sm">SSC, IGCSE and GCSE</p>
-                    <p class="italic">Kalaafaanu School, Maldives</p>
+                <div v-for="course in course" class="flex flex-col rounded-sm p-3">
+                    <p class="font-light text-white/50">{{ course.year }}</p>
+                    <p class="text-sm">{{ course.degree }}</p>
+                    <p class="italic">{{ course.institution }}</p>
                 </div>
             </div>
         </div>
--- src/components/EducationContent.vue
+++ src/components/EducationContent.vue
@@ -2,17 +2,17 @@
 const education = [
     {
         year: '2018 - 2021',
-        degree: 'BSc (Hons) in Accounting and Finance with Specialism in Taxation',
+        name: 'BSc (Hons) in Accounting and Finance with Specialism in Taxation',
         institution: 'Asia Pacific University, Malaysia'
     },
     {
         year: '2015 - 2017',
-        degree: 'Edexcel GCE & HSC',
+        name: 'Edexcel GCE & HSC',
         institution: 'Center for Higher Education, Maldives'
     },
     {
         year: '2012 - 2014',
-        degree: 'SSC, IGCSE and GCSE',
+        name: 'SSC, IGCSE and GCSE',
         institution: 'Kalaafaanu School, Maldives'
     }
 ]
@@ -23,10 +23,10 @@ const education = [
     <div class="mt-5 text-start border rounded-md text-xs w-72">
             <div>
                 <!-- Bachelors -->
-                <div v-for="course in education" class="flex flex-col rounded-sm p-3">
-                    <p class="font-light text-white/50">{{ course.year }}</p>
-                    <p class="text-sm">{{ course.degree }}</p>
-                    <p class="italic">{{ course.institution }}</p>
+                <div v-for="qualification in education" class="flex flex-col rounded-sm p-3">
+                    <p class="font-light text-white/50">{{ qualification.year }}</p>
+                    <p class="text-sm">{{ qualification.name }}</p>
+                    <p class="italic">{{ qualification.institution }}</p>
                 </div>
             </div>
         </div>
