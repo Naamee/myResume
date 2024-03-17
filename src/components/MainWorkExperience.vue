7b349ec5a7a13e108086023f64c647a3cda6ffe2
Naamee <naameeakram@gmail.com>
2024-03-17 18:58:02 +0500

    removed additional gradient & test pdf

--- src/components/MainWorkExperience.vue
+++ src/components/MainWorkExperience.vue
@@ -15,7 +15,7 @@ watch (targetIsVisible, (isVisible) => {
 
 <template>
     <div ref="target" class="flex flex-col snap-start min-h-lvh w-full items-center justify-center text-white bg-gradient-358 from-blue-950/25 from-70% to-80% to-black">
-      <div class="flex flex-col md:flex-row md:pt-10 snap-start min-h-lvh w-full items-center justify-center text-gray-200 bg-gradient-5 from-blue-950/25 from-70% to-80% to-black">
+      <div class="flex flex-col md:flex-row md:pt-10 snap-start min-h-lvh w-full items-center justify-center text-gray-200 ">
         <ContentWork/>
     </div>
     </div>
